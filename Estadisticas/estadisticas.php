c585c3def72a43f1153d356d575305731b2501be
Matias Basterra <basterramatias2020@gmail.com>
2025-12-01 01:48:57 -0300

    estadisticas

--- Estadisticas/estadisticas.php
+++ Estadisticas/estadisticas.php
@@ -16,8 +16,7 @@
     require('../includesPHP/navGeneral.php');
     ?>
     <div class="container mt-4">
-        <div class="row">
-            <div class="col-12 col-lg-6">
+            <div class="col-12 mb-4">
                 <div class="card">
                     <div class="card-header">
                         <h5 class="card-title">Estadisticas de cuestionarios completados</h5>
@@ -25,35 +24,35 @@
                     <div class="card-body">
                         <div>
                             <h6>Promedio de respuestas correctas</h6>
-                            <p><b>65.7%</b></p>
+                            <p><b id="promedioCorrectas">%</b></p>
                         </div>
                         <div>
                             <h6>Cantidad de cuestionario resueltos</h6>
-                            <p><b>25</b></p>
+                            <p><b id="cantidadCuestionarios"></b></p>
                         </div>
                         <div>
                             <h6>Categorias con mas respuestas correctas</h6>
-                            <p><b>Geografia</b></p>
+                            <p><b id="categoria"></b></p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-lg-6 mt-3">
+            <div class="col-12">
                 <div class="card">
                     <div class="card-header">
                         <h5 class="card-title">Estadisticas de cuestionarios creados</h5>
                     </div>
-                    <div class="card-body">
-                        <div>
-                            <h6 class="card-title">Cantidad de respuestas</h5>
+                    <div class="card-body text-center">
+                        <div class="mb-4">
+                            <h5 class="card-title">Cantidad de respuestas</h5>
                                 <canvas id="graficoCantRespuestas"></canvas>
                         </div>
-                        <div>
-                            <h6 class="card-title">Cantidad de usuarios</h5>
+                        <div class="mb-4">
+                            <h5 class="card-title">Cantidad de usuarios</h5>
                                 <canvas id="graficoCantUsuarios"></canvas>
                         </div>
-                        <div>
-                            <h6 class="card-title">Valoracion promedio</h5>
+                        <div class="mb-4">
+                            <h5 class="card-title">Valoracion promedio</h5>
                                 <canvas id="graficoValoracion"></canvas>
                         </div>
                     </div>
